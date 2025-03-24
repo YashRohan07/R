d85fcb71b5bdc17d758f50788853af84bb7fc0e9
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2025-03-24 08:14:11 +0600

    Add files via upload

---
+++ 01. Syntax.R
@@ -0,0 +1,26 @@
+# R is a programming language that is interpreted and dynamically typed.
+# It is widely used for statistical computing, data analysis, and graphical presentation.
+# R is a great tool for data science, machine learning, and data visualization.
+# It provides a wide range of statistical techniques, such as statistical tests, classification, clustering, and data reduction.
+# R is also known for its powerful graphical capabilities, making it easy to create plots like pie charts, histograms, box plots, scatter plots, and more.
+
+
+# You can print text by using single or double quotes:
+"Yash Rohan!"  
+
+
+# Without using print() - In an interactive session, this will automatically display the output
+"Hello, World!"  
+
+
+# Performing simple calculations
+15 + 15  # This will automatically display 30 in an interactive session.
+
+
+# If you want to explicitly print something inside curly braces or an expression,
+# you can use the print() function to display the result.
+print("Hello, World!")  # Explicitly prints the text "Hello, World!"
+
+
+# Performing simple arithmetic with print()
+print(15 + 15)  # Explicitly prints the result of 15 + 15
